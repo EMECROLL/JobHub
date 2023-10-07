654606976fa22bbbe7b15c4c5e90d6c64946c77b
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2023-10-07 16:48:48 -0500

    Creacion de rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Login from './Pages/LogIn/Login';
+import SignUp from './Pages/SignUp/SignUp';
+import OfertasLaborales from './Pages/OfertasLaborales/OfertasLaborales';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/Login" element={<Login></Login>}></Route>
+          <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
+          <Route path="/OfertasLaborales" element={<OfertasLaborales></OfertasLaborales>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
