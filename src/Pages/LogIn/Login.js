14004ac7fae719f797b276b31845fad5ee2e237b
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2023-10-12 18:24:27 -0500

    Modulo de Registrarse

--- src/Pages/LogIn/Login.js
+++ src/Pages/LogIn/Login.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>
-      <h1>Prueba</h1>
+    <div className='flex justify-center items-center h-screen bg-gray-100 border border-gray-300'>
+      <div className=''>
+        Imagen
+      </div>
+      <div className=''>
+        Iniciar Sesión
+        <form>
+          <label>Correo:</label>
+          <input type='email'></input>
+          <label>Contraseña:</label>
+          <input type='password'></input>
+        </form>
+        <button>Iniciar Sesión</button>
+        <hr></hr>
+        {/* Registrarse con Facebook y Gmail*/}
+        <button>Facebook</button>
+        <button>Gmail</button>
+      </div>
     </div>
   )
 }
