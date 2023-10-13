897b27c00d1e33936a2730cb7cfbf301e44abc4f
Maynor Bermon <maynorbermon@gmail.com>
2023-10-13 07:38:00 -0500

    Rutas

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,9 +9,11 @@ function Header() {
   return (
     <header className="bg-blue-500 p-4">
       <div className="container mx-auto flex justify-between ">
+      <Link to="/" className={hover}>
         <div>
           <img src={Logo} className="mr-2" width={80} />
         </div>
+        </Link>
         <Link to="/" className={hover}>
           <h1 className={texto}>Inicio</h1>
         </Link>
@@ -24,7 +26,7 @@ function Header() {
         <Link to="/Contacto" className={hover}>
           <h1 className={texto}>Contacto</h1>
         </Link>
-        <Link to="/Ingresar" className={hover}>
+        <Link to="/Login" className={hover}>
           <h1 className={texto}>Ingresar</h1>
         </Link>
       </div>
