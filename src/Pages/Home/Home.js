7d118320cf53f7565c333d2bc3fe13e3912acc19
Solrac619 <Solracyestom2015@gmail.com>
2023-10-14 14:57:17 -0500

    Cambios

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
 
 function Home() {
   const hover = "hover:bg-gray-800 p-2 rounded-lg";
-  return (
 
-    
-    <div className="w-full h-[1187px] relative overflow-hidden bg-white">
+  return (
+    <>
+    <Header/>
+    <div className="w-full h-[1687px] relative overflow-hidden bg-white">
       <img
         src="https://factorial.mx/wp-content/uploads/2023/02/03121201/reuniones-de-trabajo.jpg"
         className="md:w-full xl:w-full h-[640px] absolute hover:shadow-xl active:opacity-75"
@@ -23,24 +26,55 @@ function Home() {
       >
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.
       </p>
+      <p className="w-[453px] h-[66px] absolute left-[904px] top-[340px] text-5xl font-semibold text-center text-white"
+      >
+        ¿Qué es JOBHUB?
+      </p>
+      <div className="grid grid-cols-1 xl:grid-cols-2  gap-4 xl:gap-10 m-8" style={{ zoom: '100%' }}>
+    <div className="text-center relative flex justify-center transition-all top-[740px]">
+              <div className="mx-4">
+                <img src="https://apliklo.com/wp-content/uploads/2019/05/Cont%C3%A1ctanos.jpg" className="md:w-[740px] xl:w-[640px] h-[425px] hover:shadow-xl active:opacity-75 rounded-2xl mx-auto" alt="Imagen" />
+              </div>
+              <h2 className="absolute bottom-2 left-0 right-0 text-white font-bold text-5xl mb-10">
+                Contactanos
+              </h2>
+              <h3 className="absolute bottom-0 left-0 right-0 text-white text-lg mb-4">
+                ¿Tienes dudas?
+              </h3>
+            </div>
+    <div className="text-center relative flex justify-center transition-all top-[740px]">
+              <div className="mx-4">
+                <img src="https://blog.interfell.com/hubfs/como%20crear%20un%20equipo%20de%20trabajo%20fuerte%20y%20estable.jpg" className='md:w-[740px] xl:w-[640px] h-[425px] hover:shadow-xl active:opacity-75 rounded-2xl mx-auto' alt="Imagen" />
+              </div>
+              <h2 className="absolute bottom-2 left-0 right-0 text-white font-bold text-5xl mb-10">
+                Catálogo
+              </h2>
+              <h3 className="absolute bottom-0 left-0 right-0 text-white text-lg mb-4">
+                Conoce todas las ofertas!
+              </h3>
+            </div>
+            </div>
       <p
-        className="w-[auto] h-[66px] absolute left-[508px] top-[840px] text-5xl font-semibold text-center text-[#0078AB]"
+        className="w-[auto] h-[66px] absolute left-[508px] top-[1340px] text-5xl font-semibold text-center text-[#0078AB]"
       >
         ¿Tienes ofertas laborales que ofrecer?
       </p>
       <p
-        className="w-[auto] h-[66px] absolute left-[708px] top-[919px] text-5xl font-semibold text-center text-black"
+        className="w-[auto] h-[66px] absolute left-[708px] top-[1419px] text-5xl font-semibold text-center text-black"
       >
         Publicalas en JOBHUB
       </p>
       <Link to="/SignUp" className={hover}>
       <p
-        className="w-[453px] h-[66px] absolute left-[1208px] top-[1003px] text-2xl text-center text-[#2cbcc6]"
+        className="w-[453px] h-[66px] absolute left-[1208px] top-[1503px] text-2xl text-center text-[#2cbcc6]"
       >
         REGISTRAR EMPRESA
       </p>
       </Link>
     </div>
+    
+    <Footer/>
+    </>
   );
 }
 
