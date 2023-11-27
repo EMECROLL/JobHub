0c2f7bbfa611e931e0a8aa72af712e410546d28c
Manuel Alejandro Pasos Cupul <alexito210904@gmail.com>
2023-11-27 00:24:50 -0500

    Correccion formulario de Contacto

--- src/Pages/Contacto/Contacto.js
+++ src/Pages/Contacto/Contacto.js
@@ -1,42 +1,84 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import emailjs from '@emailjs/browser';
 import Header from '../../Components/Header/Header';
 import Footer from '../../Components/Footer/Footer';
 
 
 function Contacto() {
+    const [nombre, setNombre] = useState("")
+    const [mensaje, setMensaje] = useState("")
+    const [numero, setNumero] = useState("")
+    const [email, setEmail] = useState("")
+    const [mensajeEnviado,setMensajeEnviado] = useState(false)
+    const [camposVacios, setCamposVacios] = useState(false)
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+    
+    //! Validaciones
+    //! Validar que los campos no estén vacíos
+    if (!nombre || !mensaje || !numero || !email) {
+        setCamposVacios(true);
+        return; //* Detener la función si faltan campos
+    }
+    //! Validar no campos en blanco
+    if (!nombre.trim() || !mensaje.trim() || !numero.trim() || !email.trim()) {
+        setCamposVacios(true);
+        return; //* Detener la función si faltan campos
+    }
+    emailjs.sendForm('service_0urlbdi', 'template_jsyaoue', form.current, '9i9ykY91LhDEEdIY2')
+        .then((result) => {
+            console.log(result.text);
+            setNombre("")
+            setMensaje("")
+            setNumero("")
+            setEmail("")
+            setMensajeEnviado(true)
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     
     const hover = "hover: p-2 rounded-lg";
     return (
         <>
             <Header />
-                <div id="contenedor principal" className='container mx-auto'>
+                <div  id="contenedor principal" className='container mx-auto'>
                     <p className='text-center text-6xl p-4'>Contáctanos</p>
                     <div id="contenedor_form" className='mx-auto text-center bg-indigo-200 rounded-lg overflow-hidden p-4 mb-10 w-1/2'>
                         <p className='font-semibold'>¿Cómo podemos ayudarte?</p>
-                        <form className='mt-4' id="formulario_contacto" method="POST">
+                        <form ref={form} onSubmit={sendEmail} className='mt-4' >
                             <div className='mb-4'>
                                 <p className='mr-80 font-semibold'>Nombre:</p>
-                                <input type="text" name="nombre" id="formulario_nombre" placeholder="Nombre" className={`${hover} w-96`} />
+                                <input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" name="form_name" placeholder="Nombre" className={`${hover} w-96`} required/>
                             </div>
                             <p className='mr-80 font-semibold'>Mensaje:</p>
                             <div className='mb-4'>
                                 <textarea
-                                    name="mensaje"
-                                    id="formulario_mensaje"
+                                    value={mensaje}
+                                    onChange={(e) => setMensaje(e.target.value)}
+                                    name="form_message"
                                     placeholder="Mensaje"
                                     className={`${hover} h-32 w-96`}
+                                    required
                                 />
                             </div>
                             <div className='mb-4'>
                                 <p className='mr-60 font-semibold'>Número telefónico:</p>
-                                <input type="text" name="telefono" id="formulario_telefono" placeholder="Numero de teléfono" className={`${hover} w-96`} />
+                                <input onChange={(e) => setNumero(e.target.value)} value={numero} type="text" name="form_numero" placeholder="Numero de teléfono" className={`${hover} w-96`} required/>
                             </div>
                             <div className='mb-4'>
                                 <p className='mr-60 font-semibold'>Correo electrónico:</p>
-                                <input type="text" name="correo" id="formulario_correo" placeholder="Correo electrónico" className={`${hover} w-96`} />
+                                <input onChange={(e) => setEmail(e.target.value)} value={email} type="text" name="form_correo" placeholder="Correo electrónico" className={`${hover} w-96`} required/>
+                            </div>
+                            <div className='py-4'>
+                                {mensajeEnviado && <p className='text-indigo-500'>Mensaje enviado correctamente.<br></br> Por favor, revisa tu bandeja de entrada y spam</p>}
+                                {camposVacios && <p className='text-red-500 '>Por favor, complete todos los campos</p>}
                             </div>
                             <div>
-                                <button type="button" id="formulario_enviar" className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded'>Enviar</button>
+                                <button type='submit' className='bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded'>Enviar</button>
                             </div>
                         </form>
                     </div>
@@ -45,41 +87,5 @@ function Contacto() {
         </>
     );
 }
-// http://localhost:3001/verformulario
-// http://localhost:3001/enviarformulario
-document.addEventListener("DOMContentLoaded", function () {
-    const enviarFormularioBtn = document.getElementById("formulario_enviar");
-    
-
-    enviarFormularioBtn.addEventListener("click", function () {
-
-        const nombre = document.getElementById("formulario_nombre").value;
-        const mensaje = document.getElementById("formulario_mensaje").value;
-        const numero = document.getElementById("formulario_telefono").value;
-        const correo = document.getElementById("formulario_correo").value;
-
-        const data = {
-            FormNombre: nombre,
-            FormMensaje: mensaje,
-            FormNumero: numero,
-            FormCorreo: correo
-        };
-          console.log(document.getElementById("formulario_enviar"));
-        fetch('http://localhost:3001/anadirformulario', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-            .then(response => response.json())
-            .then(result => {
-                console.log('funciona esta cosa, se envio a la base de datos', result);
-            })
-            .catch(error => console.log('Error al enviar formulario', error));
-    });
-});
-
-
 
 export default Contacto;
